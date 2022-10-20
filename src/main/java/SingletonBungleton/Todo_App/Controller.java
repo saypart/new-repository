7c8ee1f011e6346350319cd1ec99633ea8d93a81
saypart <saypart@gmail.com>
2022-10-20 23:04:14 +0900

    1차 테스트

---
+++ src/main/java/SingletonBungleton/Todo_App/Controller.java
@@ -0,0 +1,12 @@
+package SingletonBungleton.Todo_App;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "To-do Application !";
+    }
+}
---
+++ src/main/java/SingletonBungleton/Todo_App/WebConfig.java
@@ -0,0 +1,7 @@
+package SingletonBungleton.Todo_App;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+}
